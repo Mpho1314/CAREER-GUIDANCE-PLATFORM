a65704ad45465fcfd4a2434ee7a5a883a99b488b
Martin Mohale <fakomohale793@gmail.com>
2025-11-07 02:57:10 -0800

    Prepare backend for Render deployment

--- backend/config/firebaseConfig.js
+++ backend/config/firebaseConfig.js
@@ -5,16 +5,13 @@ dotenv.config();
 const serviceAccount = {
   projectId: process.env.FIREBASE_PROJECT_ID,
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"), // convert \n to newlines
 };
 
-// Initialize Firebase Admin
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
-// Named exports for other modules
 export const dbAdmin = admin.firestore();
 export const adminAuth = admin.auth();
-
 export default admin;
